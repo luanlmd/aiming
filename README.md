f0fa7e01612387b33ecbd4ae337aa4717a588277
Luan Almeida <luan@atlantis.(none)>
2012-11-17 02:28:13 -0300

    starting to have fun

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ aiming
 ======
 
 FPS aim training tool
+Live at: http://sb.luanlmd.com/aiming
--- scripts.js
+++ scripts.js
@@ -1,20 +1,96 @@
+(function($){
+        $.fn.serializeObject = function()
+        {
+        	query = this.serializeArray();
+			object = {};
+			for (i in query)
+			{
+				object[query[i].name] = query[i].value
+			}
+			return object
+        };
+ })(jQuery);
+
 $(document).ready(function(){
-	var randomTime;
-	var randomSize;
-	var progressiveHardness;
-	var enableHostages;
-	var enableDistractions;
+
+	var started = false;
+	var target = $('#target');
+	var configs;
+	var misses = 0;
+	var targetShown = 0;
+	var maxDelay = 0;
+	var minDelay = 9999;
+	var mediumDelay = 0;
+	var totalDelay = 0;
+	var totalShots = 0;
+	var totalHits = 0;
+	var winWidth = $(window).width();
+	var winHeight = $(window).height();
 
 	$('#begin').click(function(){
-		randomTime = ($('#randomTime').attr('checked'));
-		randomSize = ($('randomSize').attr('checked'));
-		progressiveHardness = ($('progressiveHardness').attr('checked'));
-		enableHostages = ($('enableHostages').attr('checked'));
-		enableDistractions = ($('enableDistractions').attr('checked'));
+		configs = $('form').serializeObject();
+		console.log(configs);
 		$('#configs').fadeOut(500);
-	});
-
-	$('#target').click(function(){
+		setTimeout(function(){ nextTarget(); started = true; } ,1000);
 		
 	});
+
+	/*$('body').click(function(){
+		miss++;
+		console.log(miss);
+	});*/
+	$(document).click(function(e) {
+		if (started)
+		{
+			totalShots ++;
+			if (configs.enableSound)
+			{
+				randId = (new Date()).getTime();
+				$('body').append('<audio src="sounds/shot.ogg" autoplay="true" id="'+randId+'" type="audio/ogg" />');
+			}
+		}
+	});
+
+
+	target.click(function(){	
+		targetShot = (new Date()).getTime();
+		target.css('display','none');
+		$(this).stop();
+		delay = targetShot - targetShown;
+		totalDelay += delay;
+		mediumDelay = totalDelay / totalShots;
+		if (delay > maxDelay) { maxDelay = delay; }
+		if (delay < minDelay) { minDelay = delay; }
+		console.log(delay);
+		
+		nextTarget();
+	});
+
+	function rand(min, max)
+	{
+		return Math.floor((Math.random()*max)+min)
+	}
+
+	function nextTarget()
+	{
+		target.css('top',rand(1, winHeight-30)); 
+		target.css('left',rand(1, winWidth-30));
+		if (configs.randomSize) { target.width(rand(10,30)); target.height(rand(10,30)); }
+		if (configs.randomDelay) { setTimeout(function(){ showTarget(); },rand(1,1000)); }
+		else { showTarget(); }
+	}
+
+	function showTarget()
+	{
+		targetShown = (new Date()).getTime();
+		totalHits++;
+		target.show();
+		if (configs.movingTarget)
+		{
+			y = rand(1, winHeight-30);
+			x = rand(1, winWidth-30);
+			target.animate({'top':y, 'left':x},10000);
+		}
+	}
+
 });
